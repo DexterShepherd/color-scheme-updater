509d367b55f9abab875feb3916f584b63da60789
Dexter Shepherd <dexterjshepherd@gmail.com>
2024-09-19 10:38:22 -0700

    isloate light and dark tmux modes

--- update-tmux.ts
+++ update-tmux.ts
@@ -4,44 +4,22 @@ import { homedir } from "os";
 import path from "path";
 
 export function run() {
-  const tmuxConfigPath = path.join(homedir(), "dotfiles/.tmux.conf");
-  const dark = {
-    thm_bg: "#2A2A37",
-    thm_fg: "#dcd7ba",
-    thm_secondary: "#7E9CD8",
-  };
-  const light = {
-    thm_bg: "#f2ecbc",
-    thm_fg: "#b35b79",
-    thm_secondary: "#5d57a3",
-  };
-
-  type Theme = typeof dark | typeof light;
+  const tmuxConfigPath = path.join(homedir(), "dotfiles/.tmux-colors.conf");
+  const lightColorsPath = "~/dotfiles/.tmux-light.conf";
+  const darkColorsPath = "~/dotfiles/.tmux-dark.conf";
 
   const contents = fs.readFileSync(tmuxConfigPath, "utf8").split("\n");
 
-  function replaceTheme(theme: Theme, contents: string[]) {
-    const themeKeys = Object.keys(theme);
-    for (let i = 0; i < contents.length; i++) {
-      const line = contents[i];
-      for (const [key, value] of Object.entries(theme)) {
-        if (line.includes(`${key}=`)) {
-          contents[i] = `${key}="${value}"`;
-        }
-      }
-    }
-  }
-
   const isDark = execSync(`dark-mode status`).toString().trim() === "on";
   if (isDark) {
-    replaceTheme(dark, contents);
+    contents[0] = `source-file ${darkColorsPath}`;
   } else {
-    replaceTheme(light, contents);
+    contents[0] = `source-file ${lightColorsPath}`;
   }
 
   fs.writeFileSync(tmuxConfigPath, contents.join("\n"));
   try {
-    execSync("tmux source-file ~/.tmux.conf");
+    execSync("tmux source-file ~/dotfiles/.tmux.conf");
   } catch (e) {
     // likely not running under tmux
   }
